bc036e624fb6e22cee18f003fa977442f5dc56b4
pvieira3 <pete.vieira@gmail.com>
2013-01-29 18:13:13 -0500

    added arm COM function

--- test-code/legCOM.cpp
+++ test-code/legCOM.cpp
@@ -58,3 +58,47 @@ Vector3d hubo_plus::getCOMOfLeg(int side) {
     return legCOM;
     }
 */
+
+//Header declaration
+//  Vector3d hubo_plus::getCOMofArm(int side);
+
+Vector3d hubo_plus::getCOMofArm(int side)
+{
+    Vector3d armCOM;
+    std::vector<Eigen::Vector3d> pCOM(6);
+    double sumMasses;
+    Eigen::Isometry3d jointLoc;
+    Vector6d armAngles, mass;
+    Vector3d sumMp;
+
+    sumMp << 0, 0, 0;
+
+    pCOM[0] << -0.0347, 0.000, 0.072;
+    pCOM[1] << 0.049747912596, 0.012531116599, 0.015643664572;
+    pCOM[2] << -0.019504891350, -0.059577480789, 0.175201757627;
+    pCOM[3] << -0.012825433376, -0.007275670525, 0.171431348038;
+    pCOM[4] << -0.019870246084, -0.045969255056, -0.011506941050;
+    pCOM[5] << 0.051509407797, 0.002163982128, 0.069388069491;
+    mass << 0.826, 1.932, 2.82, 1.809, 1.634, 1.003;
+
+    getArmAngles(side, armAngles);
+
+    for (int i = 0; i < 6; i++) {
+        huboArmFK(jointLoc, armAngles, side);
+        sumMp += jointLoc * pCOM[i] * mass(i);
+        sumMasses += mass(i);
+    }
+
+    armCOM = sumMp / sumMasses;
+    return armCOM;
+}
+//Header declaration
+//  Vector3d hubo_plus::getCOMofTorso(bool batteryIn=false);
+
+Vector3d hubo_plus::getCOMofTorso()
+{
+    Vector3d torsoCOM;
+    
+    
+
+ 
