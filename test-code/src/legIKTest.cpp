a9a4e52ee8fb37dcfe1037bfc41515db45e09a1d
Pete <pete.vieira@gmail.com>
2013-02-15 21:18:12 -0500

    nothing special

--- test-code/src/legIKTest.cpp
+++ test-code/src/legIKTest.cpp
@@ -25,6 +25,10 @@ int main(int argc, char **argv)
     hubo.setJointNominalAcceleration( RAP, 0.6 );
     hubo.setJointNominalAcceleration( RAR, 0.6 );
 
+    // Set Legs to Velocity Control
+    hubo.setLegVelCtrl(LEFT);
+    hubo.setLegVelCtrl(RIGHT);
+
     // Local Variables
     Vector6d currentLeftLegAngles, currentRightLegAngles;
     Vector6d refLeftLegAngles, refRightLegAngles;
@@ -67,17 +71,26 @@ int main(int argc, char **argv)
     {
         // Get latest data from the ach channels
         hubo.update();
-
+        // Take latest time minus old time to see if a new time was received
         dt = hubo.getTime() - ptime;
+        // Get current time in seconds
         ptime = hubo.getTime();
+
+        // If new frame is received from hubo.update()
         if( dt > 0 )
         {
             i++; if(i>imax) i=0;
 
-            // Calculate error term
-            rightLegError = refRightLegAngles - hubo.getLegAngles(RIGHT, currentRightLegAngles);
+            // Get current joint angles
+            hubo.getLegAngles(RIGHT, currentRightLegAngles);
 
-            // Set leg joint velocitys
+            // Get foot pose
+            hubo.huboLegFK(rightFootTransform, currentRightLegAngles, RIGHT);
+
+            // Calculate error term
+            rightLegError = refRightLegAngles - currentRightLegAngles;
+
+            // Set leg joint velocities
             hubo.setLegVels(RIGHT, legVelKp*leftLegError);
 
             // Send commands to Hubo
@@ -90,7 +103,7 @@ int main(int argc, char **argv)
                         << "Right Leg Angles: " << currentRightLegAngles.transpose()
                         << "\nRight Leg Ref Vels: " << refRightLegAngles.transpose()
                         << "\n\nRight Leg Error: " << rightLegError.transpose()
-                        << "\n\nHeight Decrease: " << heightDecrease
+                        << "\n\nFoot Height: " << rightFootTransform(2,3)
                         << std::endl;
             }
         }
