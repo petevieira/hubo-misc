66254fe7045486a7687f422855777fe1e573289c
Pete <pete.vieira@gmail.com>
2013-02-19 01:26:33 -0500

    very simple test program

--- test-code/src/legTraj.cpp
+++ test-code/src/legTraj.cpp
@@ -4,6 +4,21 @@ int main(int argc, char **argv)
 {
     Hubo_Tech hubo;
 
-    hubo.setJointAngle(RKN, .5);
+    hubo.setJointAngle(REB, -.5);
     hubo.sendControls();
+
+    double i, imax=20;
+
+    while(true)
+    {
+        i++;
+        if(i = imax)
+        {
+            hubo.update();
+            std::cout << "REB-State: " << hubo.getJointAngleState(REB)
+                      << "REF-Refer: " << hubo.getJointAngle(REB)
+                      << std::endl;
+            i = 0;
+        }
+    }
 }
