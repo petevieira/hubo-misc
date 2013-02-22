68d0611b81ac3ae39ecfda7c2a2f75f923c33ff5
Pete <pete.vieira@gmail.com>
2013-02-21 22:17:23 -0500

    simple urdf loading test

---
+++ test-code/src/dartTest.cpp
@@ -0,0 +1,10 @@
+#include <Hubo_Tech.h>
+
+int main(int argc, char *argv[])
+{
+    Hubo_Tech hubo;
+    hubo.loadURDFModel("/home/hubo/Downloads/hubo-motion-rt/src/dart_Lite/urdf/huboplus.urdf");
+    printf("loaded\n");
+    return 0;
+
+}
