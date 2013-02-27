99cf21ecd30ad86e7fab520e0423edb879e800f7
pvieira3 <pete.vieira@gmail.com>
2013-02-27 14:42:37 -0500

    updated balance function

--- test-code/src/oneLeg.cpp
+++ test-code/src/oneLeg.cpp
@@ -1,22 +1,57 @@
 #include <Hubo_Tech.h>
+#include "kinematics/BodyNode.h"
 
 void balance(Hubo_Tech &hubo);
 void shiftToSide(int side);
 void shiftToSide2(int side);
 void setDoubleSupportLimits(Hubo_Tech &hubo);
-Vector3d getBalancePt2COM(Hubo_Tech &hubo, Vector3d Neck2COM);
+Vector3d getBalancePt2COM(Hubo_Tech &hubo, Eigen::Isometry3d world2COM);
+Vector3d getBalancePt2COMTorso(Hubo_Tech &hubo, Eigen::Isometry3d world2COM);
 void pitchAnkles(Hubo_Tech &hubo);
+void checkFKs(Hubo_Tech &hubo);
 
 int main(int argc, char **argv)
 {
     // Create Hubo_Tech object
     Hubo_Tech hubo;
-    hubo.loadURDFModel("/home/pete/Downloads/hubo-motion-rt/src/dart_Lite/urdf/huboplus.urdf");
+    hubo.loadURDFModel("/home/rapierevite/hubo-motion-rt/src/dart_Lite/urdf/huboplus.urdf");
 
-//    balance(hubo);
+    balance(hubo);
 //    shiftToSide(right);
 //    shiftToSide2(RIGHT);
-    pitchAnkles(hubo);
+//    pitchAnkles(hubo);
+//    checkFKs(hubo);
+}
+
+void checkFKs(Hubo_Tech &hubo)
+{
+    Vector6d ltLegAngles, rtLegAngles;
+    Eigen::Isometry3d ltFootTF, rtFootTF;
+    double lap, rap;
+
+    while(true)
+    {
+        std::cout << "LAP: ";
+        std::cin >> lap;
+        std::cout << "RAP: ";
+        std::cin >> rap;
+        // set leg angles all to zero
+        ltLegAngles << 0,0,0,0,lap,0;
+        rtLegAngles << 0,0,0,0,rap,0;
+
+        // get transformation to feet
+        hubo.huboLegFK(ltFootTF, ltLegAngles, LEFT);
+        hubo.huboLegFK(rtFootTF, rtLegAngles, RIGHT);
+
+        std::cout
+            << "Left  Angles: " << ltLegAngles.transpose()
+            << "\nRight Angles: " << rtLegAngles.transpose()
+            << "\nltFootTF: \n" << ltFootTF.matrix()
+            << "\nrtFootTF: \n" << rtFootTF.matrix()
+            << "\nltFootTFINV: \n" << ltFootTF.inverse().matrix()
+            << "\nrtFootTFINV: \n" << rtFootTF.inverse().matrix()
+        << std::endl;
+    }
 }
 
 void pitchAnkles(Hubo_Tech &hubo)
@@ -65,7 +100,8 @@ void pitchAnkles(Hubo_Tech &hubo)
 void balance(Hubo_Tech &hubo)
 {
     // LOCAL VARIABLES
-    Eigen::Vector3d Neck2COM, balPt2COM;
+    Eigen::Vector3d balPt2COM;
+    Eigen::Isometry3d world2COM;
     double velLAP, velLAR, velLHR;
     double velRAP, velRAR, velRHR;
     double compGainAnkleRoll = 0.001;
@@ -91,9 +127,9 @@ void balance(Hubo_Tech &hubo)
 
             setDoubleSupportLimits(hubo);
             // get center of mass vector for Hubo w.r.t. Neck
-            Neck2COM = hubo.getCOM_FullBody();
+            world2COM = hubo.getCOM_FullBody();
 
-//            balPt2COM = getBalancePt2COM(hubo, Neck2COM);
+            balPt2COM = getBalancePt2COM(hubo, world2COM);
 
             // Compute ankle joint velocities using resistant(against COM) 
             // and compliant(with ankle moments) terms
@@ -129,14 +165,14 @@ void balance(Hubo_Tech &hubo)
         }
     }
 }
-
+/*
 void shiftToSide(int side)
 {
     // Create Hubo_Tech object
     Hubo_Tech hubo;
 
     // LOCAL VARIABLES
-    Eigen::Vector3d Neck2COM;
+    Eigen::Isometry3d world2COM;
     Eigen::Isometry3d ltFootTF, rtFootTF;
     Vector6d ltLegAngles, rtLegAngles, ltLegAnglesPrev, rtLegAnglesPrev;
     double velLAP, velLAR, velLHR;
@@ -165,17 +201,17 @@ void shiftToSide(int side)
             hubo.getLegAngles(LEFT, ltLegAnglesPrev);   ///get Left leg joint angles
             hubo.huboLegFK(rtFootTF, rtLegAnglesPrev, RIGHT); ///get transformation for 'legSide' foot
             hubo.huboLegFK(ltFootTF, ltLegAnglesPrev, LEFT); ///get transformation for 'legSide' foot
-            Neck2COM = hubo.getCOM_FullBody(); ///get center of mass vector for hubo
+            world2COM = hubo.getCOM_FullBody(); ///get center of mass vector for hubo
 
             if(side == LEFT) // lean left (ie, move feet right)
             {
-                rtFootTF(1,3) += Neck2COM(1);
-                ltFootTF(1,3) = Neck2COM(1) - ltFootTF(1,3);
+                rtFootTF(1,3) += world2COM(1,3);
+                ltFootTF(1,3) = world2COM(1,3) - ltFootTF(1,3);
             }
             else if(side == RIGHT) // lean right (ie, move feet left)
             {
-                rtFootTF(1,3) = Neck2COM(1) - rtFootTF(1,3);
-                ltFootTF(1,3) += Neck2COM(0);
+                rtFootTF(1,3) = world2COM(1,3) - rtFootTF(1,3);
+                ltFootTF(1,3) += world2COM(0);
             }
             else
                 std::cout << "You didn't use a valid 'side' argument. Valid arguments are LEFT or RIGHT\n";
@@ -184,9 +220,9 @@ void shiftToSide(int side)
             hubo.huboLegIK(ltLegAngles, ltFootTF, ltLegAnglesPrev, LEFT);
 
             // Compute ankle joint velocities using resistant and compliant terms
-            velLAP = (KpCOM * Neck2COM(0)) - compGainAnklePitch * hubo.getLeftFootMy();
+            velLAP = (KpCOM * world2COM(0)) - compGainAnklePitch * hubo.getLeftFootMy();
             velLAR = (KpCOM * (ltLegAngles(5) - ltLegAnglesPrev(5))) - compGainAnkleRoll * hubo.getLeftFootMx();
-            velRAP = (KpCOM * Neck2COM(0)) - compGainAnklePitch * hubo.getRightFootMy();
+            velRAP = (KpCOM * world2COM(0)) - compGainAnklePitch * hubo.getRightFootMy();
             velRAR = (KpCOM * (rtLegAngles(5) - rtLegAnglesPrev(5))) - compGainAnkleRoll * hubo.getRightFootMx();
 
             // Compute hip joint velocities
@@ -212,7 +248,7 @@ void shiftToSide2(int side)
     Hubo_Tech hubo;
 
     // LOCAL VARIABLES
-    Eigen::Vector3d Neck2COM;
+    Eigen::Isometry3d world2COM;
     Eigen::Isometry3d ltFootTF, rtFootTF;
     Vector6d ltLegAngles, rtLegAngles, ltLegAnglesPrev, rtLegAnglesPrev;
     double velLAP, velLAR, velLHR;
@@ -292,7 +328,7 @@ void shiftToSide2(int side)
         }
     }
 }
-
+*/
 void setDoubleSupportLimits(Hubo_Tech &hubo)
 {
     double LHRcurrentMin = hubo.getJointAngleMin(LHR);
@@ -303,29 +339,48 @@ void setDoubleSupportLimits(Hubo_Tech &hubo)
     hubo.setJointAngleMax(RHR, (LHRAngle > RHRcurrentMax ? LHRAngle : RHRcurrentMax));
 }
 
-Vector3d getBalancePt2COM(Hubo_Tech &hubo, Vector3d Neck2COMVector)
+Vector3d getBalancePt2COM(Hubo_Tech &hubo, Eigen::Isometry3d world2COM)
 {
     Vector3d balPt2COM;
-    Eigen::Isometry3d Neck2BalPt, Neck2COM;
+    Eigen::Isometry3d Neck2BalPt;
     Vector6d ltLegAngles, rtLegAngles;
-    Eigen::Isometry3d ltFootTF, rtFootTF, worldOrigin;
-    double ankle2Middle = 0;//.025;
+    Eigen::Isometry3d ltFootTF, rtFootTF, world2CenterOfSP;
+    double ankle2Middle = .025;
 
     hubo.getLegAngles(LEFT, ltLegAngles);
     hubo.getLegAngles(RIGHT, rtLegAngles);
     hubo.huboLegFK(ltFootTF, ltLegAngles, LEFT);
     hubo.huboLegFK(rtFootTF, rtLegAngles, RIGHT);
 
-    worldOrigin = Eigen::Matrix4d::Identity();
+    // Create TF from Neck to center of Support Polygon
+    world2CenterOfSP = Eigen::Matrix4d::Identity();
+    world2CenterOfSP.rotate(ltFootTF.rotation());
+    world2CenterOfSP(0,3) = ((ltFootTF(0,3) + ankle2Middle) + (rtFootTF(0,3) + ankle2Middle)) / 2;
+    world2CenterOfSP(1,3) = (ltFootTF(1,3) + rtFootTF(1,3)) / 2;
+    world2CenterOfSP(2,3) = (ltFootTF(2,3) + rtFootTF(2,3)) / 2;
 
-    worldOrigin.rotate(ltFootTF.rotation());
-    worldOrigin(0,3) = ((ltFootTF(0,3) + ankle2Middle) + (rtFootTF(0,3) + ankle2Middle)) / 2;
-    worldOrigin(1,3) = (ltFootTF(1,3) + rtFootTF(1,3)) / 2;
-    worldOrigin(2,3) = (ltFootTF(2,3) + rtFootTF(2,3)) / 2;
+    balPt2COM = (world2CenterOfSP.inverse() * world2COM).translation();
+    return balPt2COM;
+}
 
-    Neck2COM = Eigen::Matrix4d::Identity();
-    Neck2COM.translate(Neck2COMVector); 
+Vector3d getBalancePt2COMTorso(Hubo_Tech &hubo, Eigen::Isometry3d world2COM)
+{
+    Vector3d balPt2COM;
+    Eigen::Isometry3d Neck2BalPt;
+    Vector6d ltLegAngles, rtLegAngles;
+    Eigen::Isometry3d ltFootTF, rtFootTF, world2CenterOfSP;
+    double ankle2Middle = .025;
 
-    balPt2COM = (Neck2BalPt.inverse() * Neck2COM).translation();
+    ltFootTF = hubo.mSkel->getNode("Body_LAP")->getWorldTransform();
+    rtFootTF = hubo.mSkel->getNode("Body_RAP")->getWorldTransform();
+
+    // Create TF from Neck to center of Support Polygon
+    world2CenterOfSP = Eigen::Matrix4d::Identity();
+    world2CenterOfSP.rotate(ltFootTF.rotation());
+    world2CenterOfSP(0,3) = ((ltFootTF(0,3) + ankle2Middle) + (rtFootTF(0,3) + ankle2Middle)) / 2;
+    world2CenterOfSP(1,3) = (ltFootTF(1,3) + rtFootTF(1,3)) / 2;
+    world2CenterOfSP(2,3) = (ltFootTF(2,3) + rtFootTF(2,3)) / 2;
+
+    balPt2COM = (world2CenterOfSP.inverse() * world2COM).translation();
     return balPt2COM;
 }
