5d502a91040a371738f7a6e913269b53895b908a
Peter Vieira <pete.vieira@gmail.com>
2013-03-27 21:56:37 -0400

    added command line arguments to fastrak-arms and fastrak-legs

--- test-code/src/fastrak-arms.cpp
+++ test-code/src/fastrak-arms.cpp
@@ -1,12 +1,76 @@
-#include <Hubo_Tech.h>
+#include <Hubo_Control.h>
 #include <iostream>
 #include <fstream>
+#include <getopt.h>
 #include "Fastrak.h"
 
+/**
+ * Prints out how to run this program
+*/
+void usage(std::ostream& ostr) {
+    ostr << 
+        "usage: fastrak-arms [OPTIONS] \n"
+        "\n"
+        "OPTIONS:\n"
+        "\n"
+        "  -l, --left           Control left arm only.\n"
+        "  -r, --right          Control right arm only.\n"
+        "  -b, --both           Control both arms.\n"
+        "  -V, --verbose        Show output.\n"
+        "  -H, --help           See this message\n";
+}
+
+/**
+ * Main function that loops reading the sensors and commanding
+ * Hubo's arm joints based on the poses of the hands
+*/
 int main(int argc, char **argv)
 {
-    Hubo_Tech hubo;
-//    hubo_plus hubo("twoArmFastrak");
+    // check if no arguments given, if not report usage
+    if (argc != 2)
+    {
+        usage(std::cerr);
+        return 1;
+    }
+
+    bool print = false; // whether to print output or not
+    bool left = false; // whether to set left arm angles
+    bool right = false; // whether to set right arm angles
+
+    // command line lone options
+    const struct option long_options[] = 
+    {
+        { "left",       no_argument, 0, 'l' },
+        { "right",      no_argument, 0, 'r' },
+        { "both",       no_argument, 0, 'b' },
+        { "verbose",    no_argument, 0, 'V' },
+        { "help",       no_argument, 0, 'H' },
+        { 0,            0,           0,  0  },
+    };
+
+    // command line short options
+    const char* short_options = "lrbVH";
+
+    // command line option and option index number
+    int opt, option_index;
+
+    // loop through command line options and set values accordingly
+    while ( (opt = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1 )
+    {
+        switch (opt)
+        {
+            case 'l': left = true; break;
+            case 'r': right = true; break;
+            case 'b': left = true; right = true; break;
+            case 'V': print = true; break;
+            case 'H': usage(std::cout); exit(0); break;
+            default:  usage(std::cerr); exit(1); break;
+        }
+    }
+
+    // Create Hubo_Control object
+    Hubo_Control hubo;
+//    Hubo_Control hubo("fastrak-arms");
 
     // Create Fastrak object
     Fastrak fastrak;
@@ -18,15 +82,12 @@ int main(int argc, char **argv)
     // Local Variables 
     Vector6d rActualAngles, rArmAnglesCurrent, rArmAnglesNext, checkr, armNomAcc;
     Vector6d lActualAngles, lArmAnglesNext, lArmAnglesCurrent, checkl;
-    Vector3d lArmTrans, ltransEE, lArmCurrent, lArmFastrak;
-    Vector3d rArmTrans, rtransEE, rArmCurrent, rArmFastrak; 
-    Vector3d waistTrans, waistCurrent, waistFastrak;
-    Eigen::Matrix3d lRotInitial, rRotInitial, lRot, rRot, waistRot, waistRotCurrent;
+    Vector3d lArmTrans, ltransEE, lTransInitial, lArmFastrak;
+    Vector3d rArmTrans, rtransEE, rTransInitial, rArmFastrak; 
+    Eigen::Matrix3d lRotInitial, rRotInitial, lRot, rRot;
     Eigen::Isometry3d lcurrEE, rcurrEE, lTransf, rTransf, lHandCurrent, rHandCurrent, B, Br;
-    double angle, waistPos, ptime, waistVel;
-    double waistGain = 0.8;
-    int i=0, imax=4;
-    double dt;
+    int i=0, imax=40;
+    double dt, ptime;
 
     // Define arm nominal acceleration 
     armNomAcc << 0.3, 0.3, 0.3, 0.3, 0.3, 0.3;
@@ -39,25 +100,25 @@ int main(int argc, char **argv)
     hubo.sendControls();
 
     // Get initial Fastrak sensors location and orientation
-    fastrak.getPose( lArmCurrent, lRotInitial, 3, true );
-    fastrak.getPose( rArmCurrent, rRotInitial, 4, false );
+    fastrak.getPose( lTransInitial, lRotInitial, 3, true );
+    fastrak.getPose( rTransInitial, rRotInitial, 4, false );
 
     // Define starting joint angles for the arms 
-    lArmAnglesNext << 0, -.3, 0, -M_PI/2, 0, 0;
-    rArmAnglesNext << 0, .3, 0, -M_PI/2, 0, 0;
+//    lArmAnglesNext << 0, -.3, 0, -M_PI/2, 0, 0;
+//    rArmAnglesNext << 0, .3, 0, -M_PI/2, 0, 0;
 
     // Set the arm joint angles and send commands to the control daemon
-    hubo.setLeftArmAngles( lArmAnglesNext, true );
-    hubo.setRightArmAngles( rArmAnglesNext, true );
+//    hubo.setLeftArmAngles( lArmAnglesNext, true );
+//    hubo.setRightArmAngles( rArmAnglesNext, true );
 
     // While the norm of the right arm angles is greater than 0.075
     // keep waiting for arm to get to desired position
-    while ((lArmAnglesNext - checkl).norm() > 0.075 && (rArmAnglesNext - checkr).norm() > 0.075)
-    {
-        hubo.update(); // Get latest data from ach channels
-        hubo.getLeftArmAngles(checkl); // Get current left arm joint angles
-        hubo.getRightArmAngles(checkr); // Get current right arm joint angles
-    }
+//    while ((lArmAnglesNext - checkl).norm() > 0.075 && (rArmAnglesNext - checkr).norm() > 0.075)
+//    {
+//        hubo.update(); // Get latest data from ach channels
+//        hubo.getLeftArmAngles(checkl); // Get current left arm joint angles
+//        hubo.getRightArmAngles(checkr); // Get current right arm joint angles
+//    }
 
     // Get current pose of the hands
     hubo.huboArmFK(lcurrEE, lArmAnglesNext, LEFT);
@@ -77,7 +138,6 @@ int main(int argc, char **argv)
 
         if(dt>0)
         {
-            i++; if(i>imax) i=0;
 
             // get current joint angles
             hubo.getLeftArmAngles(lArmAnglesCurrent);
@@ -91,16 +151,9 @@ int main(int argc, char **argv)
             fastrak.getPose( lArmFastrak, lRot, 3, true );
             fastrak.getPose( rArmFastrak, rRot, 4, false );
 
-            // extract z-rotation angle from waistRot
-    //        waistZRot = waistRot.eulerAngles(
-    //        waistTrans = waistFastrak - waistCurrent;
-
             // compute Fastrak relative translations
-            lArmTrans = lArmFastrak - lArmCurrent;
-            rArmTrans = rArmFastrak - rArmCurrent;
-
-            // compute Fastrak relative rotation
-    //        lRot = lRotInitial.inv() * lRot;
+            lArmTrans = lArmFastrak - lTransInitial;
+            rArmTrans = rArmFastrak - rTransInitial;
 
             // create 4d identity matrix
             lTransf = Eigen::Matrix4d::Identity();
@@ -110,50 +163,35 @@ int main(int argc, char **argv)
             // pretranslate by the relative fastrak translation
             lTransf.translate(lArmTrans + ltransEE);
             rTransf.translate(rArmTrans + rtransEE);
-    //        lTransf.translate(ltransEE);
-    //        lTransf.translate(lArmTrans);
-    //        rTransf.translate(rArmTrans);
 
             // add rotation matrix to top-left corner
             lTransf.rotate(lRot);
             rTransf.rotate(rRot);
 
-            // get final transformation for arms
-    //        lTransf = lHandCurrent * lTransf;
-    //        rTransf = rHandCurrent * rTransf;
-
             // get joint angles corresponding to transformations
             hubo.huboArmIK( lArmAnglesNext, lTransf, lArmAnglesCurrent, LEFT );
             hubo.huboArmIK( rArmAnglesNext, rTransf, rArmAnglesCurrent, RIGHT );
-    //        angle = 90.0*M_PI/180.0;
 
-//            waistVel = atan2(waistRot(1,0),waistRot(0,0));
+            // set and get joint angles
+            if( left==true )
+            {
+                hubo.setLeftArmAngles( lArmAnglesNext, false );
+                hubo.getLeftArmAngles( lActualAngles );
+            }
 
-//            hubo.setJointAngle(WST, waistVel*waistGain);
-
-    //    	B(0,0) = 1; B(0,1) = 0; B(0,2) = 0; B(0,3) = ltransEE(0);
-    //        B(1,0) = 0; B(1,1) = cos(angle); B(1,2) = -sin(angle); B(1,3) = ltransEE(1);
-    //        B(2,0) = 0; B(2,1) = sin(angle); B(2,2) = cos(angle); B(2,3) = ltransEE(2);
-    //        B(3,0) = 0; B(3,1) = 0; B(3,2) = 0; B(3,3) = 1;
-
-    //    	hubo.huboArmIK( lArmAnglesNext, B, lArmAnglesCurrent, LEFT );
-    //    	hubo.huboArmIK( lArmAnglesNext, lTransf, lArmAnglesCurrent, LEFT );
-    //    	hubo.huboArmIK( rArmAnglesNext, rTransf, rArmAnglesCurrent, RIGHT );
-    //    	hubo.huboArmIK( rArmAnglesNext, B, rArmAnglesCurrent, RIGHT );
-
-            // set joint angles
-            hubo.setLeftArmAngles( lArmAnglesNext, false );
-            hubo.setRightArmAngles( rArmAnglesNext, false );
-            hubo.getLeftArmAngles( lActualAngles);
-            hubo.getRightArmAngles( rActualAngles);
-    //        hubo.huboArmFK(lHandCurrent, lArmAnglesNext, LEFT);    
-    //        hubo.huboArmFK(rHandCurrent, rArmAnglesNext, RIGHT);    
+            if( right==true )
+            {
+                hubo.setRightArmAngles( rArmAnglesNext, false );
+                hubo.getRightArmAngles( rActualAngles );
+            }
 
             // send control references
             hubo.sendControls();
-            if( i==imax )
+
+            // print output
+            if( i==imax && print==true )
             {
-/*                std::cout << "\033[2J"
+                std::cout << "\033[2J"
                           << "Fastraktl: \n" << lArmTrans
                           << "\nFastrakl: \n" << lRot
                           << "Fastraktr: \n" << rArmTrans
@@ -162,12 +200,12 @@ int main(int argc, char **argv)
                           << "\nHuboOrient: \n" << lHandCurrent.matrix()
                           << "\nlArmAnglesl: " << lActualAngles.transpose()
                           << "\nrArmAnglesl: " << rActualAngles.transpose()
-                          << "\nwaistLoc: " << waistTrans.transpose()
                           << "\nRight hand torques: " << hubo.getRightHandMx() << ", " << hubo.getRightHandMy()
                           << "\nLeft hand torques: " << hubo.getLeftHandMx() << ", " << hubo.getLeftHandMy()
                           << std::endl;
                           i = 0;
-*/            }
+            }
+            i++;
         }
     }
 }
