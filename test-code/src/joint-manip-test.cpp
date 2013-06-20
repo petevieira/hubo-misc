002852c76289a8d21ee4147a052f5751d152fcc6
Pete <pete.vieira@gmail.com>
2013-06-20 13:11:41 -0400

    added joint-manip-test to test new handle_angles function in manip-daemon

---
+++ test-code/src/joint-manip-test.cpp
@@ -0,0 +1,43 @@
+#include <manip.h>
+
+int main(int argc, char** argv)
+{
+
+    // Open up ach channel
+    ach_channel_t chan_manip_cmd;
+
+    ach_status_t r = ach_open( &chan_manip_cmd, CHAN_HUBO_MANIP_CMD, NULL );
+    daemon_assert( r==ACH_OK, __LINE__ );
+
+    // Initialize manip_cmd struct
+    hubo_manip_cmd_t manip_cmd;
+    memset( &(manip_cmd), 0, sizeof(hubo_manip_cmd_t) );
+
+    // Set components of manip_cmd struct for straight joint angles use
+    for(int side=0; side<2; side++)
+    {
+        manip_cmd.m_mode[side] = MC_ANGLES;
+        manip_cmd.m_ctrl[side] = MC_NONE;
+        manip_cmd.m_grasp[side] = MC_GRASP_LIMP;
+        manip_cmd.interrupt[side] = false;
+    }
+
+    // Set static variables for manip_cmd
+    manip_cmd.waistAngle = 0.0;
+    manip_cmd.stopNorm = 1e-10;
+    manip_cmd.convergeNorm = 0.0075;
+
+    // Set joint angles for both arms
+    fprintf(stderr, "Arm Joint Count: %d\n", ARM_JOINT_COUNT);
+//    double ltArmAngles[ARM_JOINT_COUNT];
+//    double rtArmAngles[ARM_JOINT_COUNT];
+
+//    ltArmAngles = {-M_PI/2, M_PI/2, -M_PI/2, 0, 0, 0, 0, 0, 0, 0};
+//    rtArmAngles = {-M_PI/2, -M_PI/2, -M_PI/2, 0, 0, 0, 0, 0, 0, 0};
+
+//    manip_cmd.arm_angles[LEFT][ARM_JOINT_COUNT] = ltArmAngles;
+//    manip_cmd.arm_angles[RIGHT][ARM_JOINT_COUNT] = rtArmAngles;
+
+//    r = ach_put( &chan_manip_cmd, &manip_cmd, sizeof(hubo_manip_cmd_t) );
+//    fprintf(stderr, "ach_put manip_cmd result: %s\n", ach_result_to_string(r));
+}
